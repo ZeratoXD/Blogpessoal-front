7564be64b817115684b2268f6df30673c76706d7
ZeratoXD <ivan_cassio_dias@hotmail.com>
2024-05-03 11:15:43 -0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
+import Login from './paginas/Login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Home from './paginas/home/Home';
+import { AuthProvider } from './context/AuthContext';
 
 
 function App() {
